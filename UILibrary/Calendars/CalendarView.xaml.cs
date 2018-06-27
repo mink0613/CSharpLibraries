5df72bc33107bf5154dd3ae741059203054f0af3
Min Gyung Kwak <mink0613@sae-a.com>
2018-06-27 17:03:04 +0900

    Implement CalendarView & ViewModel

---
+++ UILibrary/Calendars/CalendarView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.Composition;
+using System.Windows;
+
+namespace UILibrary.Calendars
+{
+    /// <summary>
+    /// CalendarView.xaml에 대한 상호 작용 논리
+    /// </summary>
+    [Export(typeof(CalendarView))]
+    public partial class CalendarView : Window
+    {
+        public CalendarViewModel ViewModel
+        {
+            get
+            {
+                return DataContext as CalendarViewModel;
+            }
+        }
+        
+        public CalendarView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ UILibrary/Calendars/CalendarViewModel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Windows.Input;
+using UILibrary.Base;
+
+namespace UILibrary.Calendars
+{
+    [Export(typeof(CalendarViewModel))]
+    public class CalendarViewModel : BaseViewModel
+    {
+        private ICommand _okClick;
+
+        private ICommand _cancelClick;
+
+        private DateTime _selected;
+
+        public event Action<DateTime> RequestSelect;
+
+        public event Action RequestClose;
+
+        public ICommand OKClick
+        {
+            get
+            {
+                return _okClick;
+            }
+        }
+
+        public ICommand CancelClick
+        {
+            get
+            {
+                return _cancelClick;
+            }
+        }
+
+        public DateTime Selected
+        {
+            get
+            {
+                return _selected;
+            }
+            set
+            {
+                _selected = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void OnOKClick()
+        {
+            if (RequestClose != null)
+            {
+                RequestSelect(Selected);
+                RequestClose();
+            }
+        }
+
+        private void OnCancelClick()
+        {
+            if (RequestClose != null)
+            {
+                RequestClose();
+            }
+        }
+
+        private void Initialize()
+        {
+            Selected = DateTime.Today;
+            _okClick = new RelayCommand((param) => OnOKClick(), true);
+            _cancelClick = new RelayCommand((param) => OnCancelClick(), true);
+        }
+
+        public void SetSelectedDate(DateTime date)
+        {
+            Selected = date;
+        }
+
+        [ImportingConstructor]
+        public CalendarViewModel()
+        {
+            Initialize();
+        }
+    }
+}
