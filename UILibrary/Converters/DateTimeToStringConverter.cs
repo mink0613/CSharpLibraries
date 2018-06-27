50e96f5819846c8d8f99ee05de4e3a137123361f
Min Gyung Kwak <mink0613@sae-a.com>
2018-06-27 14:53:48 +0900

    Implement DateTimeToStringConverter

---
+++ UILibrary/Converters/DateTimeToStringConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace UILibrary.Converters
+{
+    [ValueConversion(typeof(DateTime), typeof(string))]
+    public class DateTimeToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime time = (DateTime)value;
+            return time.ToShortDateString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
