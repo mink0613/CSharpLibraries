eff055c7236fefb9790f392a21216140f736d944
Min Gyung Kwak <mink0613@sae-a.com>
2018-08-29 01:59:02 -0700

    Impliment Multi Value Converter

---
+++ UILibrary/Converters/MultipleBoolsToBoolConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace UILibrary.Converters
+{
+    [ValueConversion(typeof(bool), typeof(bool))]
+    public class MultipleBoolsToBoolConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            foreach (bool value in values)
+            {
+                if (value == false)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public object[] ConvertBack(object values, Type[] targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
