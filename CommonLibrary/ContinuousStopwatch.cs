42f3763e4f1bc38c6e7cb8470e0883d253489a67
Min Gyung Kwak <mink0613@sae-a.com>
2019-01-15 11:49:13 +0900

    Implement ContinuousStopwatch

---
+++ CommonLibrary/ContinuousStopwatch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace CommonLibrary
+{
+    public class ContinuousStopwatch : Stopwatch
+    {
+        public TimeSpan Startoffset
+        {
+            get;
+            private set;
+        }
+
+        public ContinuousStopwatch(TimeSpan timeSpan)
+        {
+            Startoffset = timeSpan;
+        }
+
+        public new TimeSpan Elapsed
+        {
+            get
+            {
+                return Startoffset + base.Elapsed;
+            }
+        }
+
+        public new long ElapsedMilliseconds
+        {
+            get
+            {
+                return (long)Startoffset.TotalMilliseconds + base.ElapsedMilliseconds;
+            }
+        }
+    }
+}
