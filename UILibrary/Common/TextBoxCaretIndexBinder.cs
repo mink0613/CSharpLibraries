74de78ad0e5b69a02fb8e93bd8c0ef877dcbbe4f
Min Gyung Kwak <mink0613@sae-a.com>
2019-02-15 17:13:39 +0900

    Add TextBoxCaretIndexBinder

---
+++ UILibrary/Common/TextBoxCaretIndexBinder.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+
+namespace UILibrary.Common
+{
+    public class TextBoxCaretIndexBinder : DependencyObject
+    {
+        public static readonly DependencyProperty CursorPositionProperty =
+            DependencyProperty.Register(
+                "CursorPosition",
+                typeof(int),
+                typeof(TextBoxCaretIndexBinder),
+                new FrameworkPropertyMetadata(
+                    default(int),
+                    new PropertyChangedCallback(CursorPositionChanged)));
+
+        public static void SetCursorPosition(DependencyObject dependencyObject, int i)
+        {
+            dependencyObject.SetValue(CursorPositionProperty, i);
+        }
+
+        public static int GetCursorPosition(DependencyObject dependencyObject)
+        {
+            return (int)dependencyObject.GetValue(CursorPositionProperty);
+        }
+
+        private static void CursorPositionChanged(
+            DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
+        {
+        }
+    }
+}
