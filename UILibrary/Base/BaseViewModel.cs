2c392a9a53226c998e7ed4ba3929c1be25ffa3d7
Min Gyung Kwak <mink0613@sae-a.com>
2019-08-01 17:51:31 +0900

    Implement OKClicked & Closing for BaseViewModel

--- UILibrary/Base/BaseViewModel.cs
+++ UILibrary/Base/BaseViewModel.cs
@@ -1,11 +1,17 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using UILibrary.Common;
 
 namespace UILibrary.Base
 {
     public abstract class BaseViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+
+        public event EventHandler<GenericEventArgs<object>> OKClicked;
+
+        public event EventHandler Closing;
 
         public void SetValue<T>(ref T property, T value, [CallerMemberName] string propertyName = null)
         {
